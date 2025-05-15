fd94ffa19cf4be0dcd0b1678fda006d56da3ae07
JulioVianaDev <JulioVianaDev@gmail.com>
2025-05-14 21:55:49 -0300

    correção  do easy

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,7 +58,13 @@ export default {
 					dark: '#121212', // Even darker for better contrast
 					background: '#F9FAFB',
 				}
-			}
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				'fade-in': 'fade-in 0.5s ease-out',
+				'fade-in-right': 'fade-in-right 0.5s ease-out'
+			},
 		}
 	},
 	plugins: [require("tailwindcss-animate")],
